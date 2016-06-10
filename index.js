d5d28c63406b5a17c3dedb2a5a36b1c15bddcb14
Dejan Levec <dejan@dejanlevec.com>
2016-06-10 08:59:42 +0200

    console.log messages

--- index.js
+++ index.js
@@ -8,18 +8,22 @@ var tcpClients = [];
 var server = net.createServer(function(socket) {
     tcpClients.push(socket);
 
+    console.log('tcp client connected');
     io.emit('tcp connected', socket.name);
 
     socket.on('data', function (data) {
+        console.log('tcp client data: ', data);
         io.emit('tcp message', data);
     });
 
     socket.on('end', function () {
+        console.log('tcp client disconnected');
         tcpClients.splice(tcpClients.indexOf(socket), 1);
         io.emit('tcp disconnected', socket.name);
     });
 
     socket.on('error', function (err) {
+        console.log('tcp client socket error');
         tcpClients.splice(tcpClients.indexOf(socket), 1);
         io.emit('tcp disconnected', socket.name);
     });
