eea18235cd27044715b9a49850fb35a771504bee
Dejan Levec <dejan@dejanlevec.com>
2016-06-10 08:57:32 +0200

    re-indent

--- index.js
+++ index.js
@@ -14,28 +14,28 @@ var server = net.createServer(function(socket) {
         io.emit('tcp message', data);
     });
 
-  socket.on('end', function () {
-    tcpClients.splice(tcpClients.indexOf(socket), 1);
-    io.emit('tcp disconnected', socket.name);
-  });
+    socket.on('end', function () {
+        tcpClients.splice(tcpClients.indexOf(socket), 1);
+        io.emit('tcp disconnected', socket.name);
+    });
 });
 
 server.listen(6162, '0.0.0.0');
 
 app.get('/', function(req, res){
-  res.sendFile(__dirname + '/index.html');
+    res.sendFile(__dirname + '/index.html');
 });
 
 io.on('connection', function(socket){
-  socket.on('chat message', function(msg){
-    io.emit('chat message', msg);
+    socket.on('chat message', function(msg){
+        io.emit('chat message', msg);
 
-    tcpClients.forEach(function (client) {
-        client.write(msg);
+        tcpClients.forEach(function (client) {
+            client.write(msg);
+        });
     });
-  });
 });
 
 http.listen(6161, function(){
-  console.log('listening on *:6161');
+    console.log('listening on *:6161');
 });
